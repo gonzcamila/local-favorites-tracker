f9e3f768a0c3e25af9916b3804b83d3ab9ea9683
gonzcamila <CAMILA.GONZALEZ@TCU.EDU>
2026-09-25 11:10:20 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log('app.js connected');
+
+let myFavorite = {
+    name: 'Starbucks on University Drive',
+    category: 'coffee',
+    rating: 5,
+    notes: 'Great study spot with fast wifi',
+    dateAdded: new Date().toLocaleDateString()
+};
+
+console.log(myFavorite);
+console.log(typeof myFavorite.name);
+console.log(typeof myFavorite.rating);
+
